469488a04d28d952b4b545cb1c35800bdbb68548
hasanAntakly <105155021+hasanAntakly@users.noreply.github.com>
2022-09-17 22:40:51 +0300

    a7a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainComponent } from './admin/main/main.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+		MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
